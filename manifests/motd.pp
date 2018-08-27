b76aa5271e2253246de96941b418e2bdc39de30c
Michael Miller <michael@autostructure.io>
2018-08-27 14:06:20 -0400

    Lab 2.3

--- manifests/motd.pp
+++ manifests/motd.pp
@@ -1,4 +1,12 @@
 #lab 2.2
 class practitioner::motd {
-
+  file { '/etc/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => epp('practitioner/motd.epp'),
+    # if variables are included in epp file
+    # content => epp('practitioner/motd.epp', var1 => value)
+  }
 }
