d77e3b6facf5989cde1882438c2800cb9cbc118d
Luther <luther@autostructure.io>
2018-08-27 14:12:45 -0400

    template stuff

--- manifests/motd.pp
+++ manifests/motd.pp
@@ -1,3 +1,10 @@
+# Hey f you dude
 class practitioner::motd {
-
+  file {'/etc/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => 'practitioner/motd.epp',
+    }
 }
