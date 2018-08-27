f32b8e981ad09497f7c01200e6e9b95bc9532da0
Michael Miller <michael@autostructure.io>
2018-08-27 16:21:06 -0400

    Lab 4.1 V1

---
+++ manifests/hosts.pp
@@ -0,0 +1,10 @@
+#lab 4.1
+class practitioner::hosts {
+  resources { 'host':
+    purge => true,
+  }
+  host { 'localhost':
+    ensure       => present,
+    host_aliases => 'localhost.localdomain',
+  }
+}
