ffd14abf72824205ba3d41c0b0109d59fa65f5d3
Michael Miller <michael@autostructure.io>
2018-08-28 15:15:10 -0400

    Lab 6.2

---
+++ manifests/classroom.pp
@@ -0,0 +1,13 @@
+#Lab 6.2
+class practitioner::classroom {
+  # export a virtual host resource for yourself
+  @@host { $::fqdn:
+    ensure       => 'present',
+    host_aliases => [$::hostname],
+    ip           => $::ipaddress,
+    tag          => 'classroom',
+  }
+  # collect all resources from the database (including your own)
+  # enforce only those tagged with `classroom`.
+  Host <<| tag == 'classroom' |>>
+}
