467042f421ffa176ec222bd21148730f94557db9
Luther <luther@autostructure.io>
2018-08-28 14:22:19 -0400

    drftugy

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,8 +1,10 @@
 #blah
+class practitioner::mysql {
 class { '::mysql::server':
   root_password => 'strongpassword',
 },
 class { '::mysql::bindings':
   php_enable  => true,
   perl_enable => true,
+},
 },
