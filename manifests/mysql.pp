3dd9181b866cabbdc5e9f11d54e203140272e0e9
Luther <luther@autostructure.io>
2018-08-28 14:30:04 -0400

    evfdcs

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,10 +1,12 @@
 #blah
 class practitioner::mysql {
-class { '::mysql::server':
-  root_password => 'strongpassword',
+  class { '::mysql::server':
+    root_password => 'strongpassword',
 },
-class { '::mysql::bindings':
-  php_enable  => true,
-  perl_enable => true,
+  class { '::mysql::bindings':
+    php_enable  => true,
+    perl_enable => true,
 },
+contain mysql::bindings
+contain mysql::server
 }
