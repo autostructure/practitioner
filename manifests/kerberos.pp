ce7bb3ea16856bd53ea5020a3c6ee94f57235efa
Luther <luther@autostructure.io>
2018-08-28 17:24:44 -0400

    iougyf

--- manifests/kerberos.pp
+++ manifests/kerberos.pp
@@ -1,5 +1,5 @@
 #google
-class kerberos {
+class practitioner::kerberos {
   augeas {'krb5.conf':
     context => '/files/etc/krb5.conf/libdefaults',
     changes => 'set default_realm PUPPETLABS.VM',
