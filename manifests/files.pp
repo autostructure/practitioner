59cae8f84cf797d8e8c50496a6e2647f6f6f16ea
Luther <luther@autostructure.io>
2018-08-28 17:06:24 -0400

    iouhjh

---
+++ manifests/files.pp
@@ -0,0 +1,28 @@
+# boo
+class practitioner::files {
+file_line {'allow chron jobs':
+  ensure => present,
+  path   => '/etc/cron.allow',
+  line   => 'root'
+}
+file_line { 'prevent cron jobs':
+ensure => present,
+path   => '/etc/cron.deny',
+line   => '*',
+}
+concat { '/etc/motd':
+owner => 'root',
+group => 'root',
+mode  => '0644',
+}
+concat::fragment { 'motd header':
+target  => '/etc/motd',
+order   => '01',
+content => epp('files/motd_header.epp'),
+}
+concat::fragment { 'sample motd message':
+target  => '/etc/motd',
+order   => '50',
+content => "This is a sample motd message\n",
+}
+}
