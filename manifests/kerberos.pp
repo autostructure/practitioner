cdc5332570d0c6445efa92a768cb571af78d51de
Luther <luther@autostructure.io>
2018-08-28 17:29:41 -0400

    hgvghffty

--- manifests/kerberos.pp
+++ manifests/kerberos.pp
@@ -1,5 +1,8 @@
 #google
 class practitioner::kerberos {
+  file {'chron.allow':
+    ensure  => present,
+  }
   augeas {'krb5.conf':
     context => '/files/etc/krb5.conf/libdefaults',
     changes => 'set default_realm PUPPETLABS.VM',
