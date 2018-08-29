e1be5d6f5cd6ddae7531edd682624d6420d812b1
Luther <luther@autostructure.io>
2018-08-29 09:41:39 -0400

    tfvbygnhy

---
+++ manifests/inherit.pp
@@ -0,0 +1,12 @@
+#woof
+class practitioner::wordpress {
+  include wordpress
+# override load balancer to 'wordpress' listening service
+Haproxy::Balancermember[$facts['fqdn']] {
+  listening_service => 'wordpress',
+}
+  class {'webapp':
+    docroot  => '/opt/wordpress',
+    app_name => 'wordpress',
+    }
+}
---
+++ manifests/wordpress.pp
@@ -0,0 +1,20 @@
+#hi
+class practitioner::webapp (
+  String $app = 'webapp'
+  ){
+  include mysql::server
+  class { 'mysql::bindings':
+    php_enable => true,
+  }
+include apache
+include apache::mod::php
+  apache::vhost { $facts['fqdn']:
+    priority   => '10',
+    vhost_name => $facts['fqdn'],
+    port       => '80',
+    docroot    => '/var/www/html',
+}
+  @@haproxy::balancermember { $facts['fqdn']:
+    listening_service => $app,
+}
+}
