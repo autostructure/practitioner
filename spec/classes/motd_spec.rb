a2d50ce52d383cce16f2d8d1603c4f5832158b13
Michael Miller <michael@autostructure.io>
2018-08-29 13:19:54 -0400

    create spect test for motd

---
+++ spec/classes/motd_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'practitioner::motd' do
+  context 'with defaults' do
+    let(:facts) { { 'operatingsystem' => 'Centos'} }
+
+    it {
+      is_expected.to contain_file('/etc/motd').with(
+        'ensure'  => 'file',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+      )
+    }
+  end
+end
